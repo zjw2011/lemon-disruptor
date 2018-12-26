3ef01574948966394cdf162a04cd8933801e778a
sanye <909861882@qq.com>
2018-12-26 15:42:54 +0800

    增加示例代码

---
+++ lemon-disruptor-sample/src/main/java/org/lemonframework/disruptor/sample/SampleApplication.java
@@ -0,0 +1,19 @@
+package org.lemonframework.disruptor.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * startup.
+ *
+ * @author jiawei zhang
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+}
